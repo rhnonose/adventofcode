370f8d06dd58e416466795baac5509a2110746e3
Rodrigo nonose <rodrigo.nonose@matera.com>
2015-12-29 11:47:36 -0200

    Day 5-2 on adventofcode.com

--- lib/nice_naughty.ex
+++ lib/nice_naughty.ex
@@ -5,36 +5,52 @@ defmodule NiceNaughty do
     NiceNaughty.Supervisor.start_link() 
   end
 
-  def process_input(path) do
+  def process_input(path, part) do
     path |>
     File.open! |>
     IO.binread(:all) |>
     String.split("\n") |>
-    count_nice
+    count_nice(part)
   end
 
-  def is_nice do
+  def is_nice?(:part1) do
     fn string ->
-      has_three_vowels(string) &&
-      has_consecutive_letters(string) &&
-      !has_some_strings(string)
+      has_three_vowels?(string) &&
+      has_consecutive_letters?(string) &&
+      !has_some_strings?(string)
     end
   end
 
-  def has_three_vowels(string) do
+  def is_nice?(:part2) do
+    fn string ->
+      has_not_overlapped_pairs?(string) &&
+      has_repeated_letter_with_middleman?(string)
+    end
+  end
+
+  def has_three_vowels?(string) do
     Regex.match?(~r/.*[aeiou].*[aeiou].*[aeiou].*/i, string)
   end
 
-  def has_consecutive_letters(string) do
+  def has_consecutive_letters?(string) do
     Regex.match?(~r/(.)\1/i, string)
   end
 
-  def has_some_strings(string) do
+  def has_some_strings?(string) do
     Regex.match?(~r/.*(ab|cd|pq|xy).*/i, string)
   end
 
-  def count_nice(strings) do
-    {nice, _naughty} = Enum.partition(strings, is_nice)
+  def has_not_overlapped_pairs?(string) do
+    Regex.match?(~r/(..).*\1/i, string)
+  end
+
+  def has_repeated_letter_with_middleman?(string) do
+    Regex.match?(~r/(.).\1/i, string)
+  end
+
+  def count_nice(strings, part) do
+    {nice, _naughty} = Enum.partition(strings, is_nice?(part))
     Enum.count(nice)
   end
+
 end
--- test/nice_naughty_test.exs
+++ test/nice_naughty_test.exs
@@ -3,43 +3,62 @@ defmodule NiceNaughtyTest do
   doctest NiceNaughty
 
   test "at least three vowels" do
-    assert  NiceNaughty.has_three_vowels("jfmthnfrtncutdie")
-    assert  NiceNaughty.has_three_vowels("jfmthnfrtucutdie")
-    assert  NiceNaughty.has_three_vowels("jaethnfrtucutdie")
-    assert !NiceNaughty.has_three_vowels("zztdcqzqddaazdjp")
+    assert  NiceNaughty.has_three_vowels?("jfmthnfrtncutdie")
+    assert  NiceNaughty.has_three_vowels?("jfmthnfrtucutdie")
+    assert  NiceNaughty.has_three_vowels?("jaethnfrtucutdie")
+    assert !NiceNaughty.has_three_vowels?("zztdcqzqddaazdjp")
   end
 
   test "at least one letter in a row" do
-    assert  NiceNaughty.has_consecutive_letters("eytquncjituzzhsx")
-    assert  NiceNaughty.has_consecutive_letters("dtfkgggvqadhqbwb")
-    assert  NiceNaughty.has_consecutive_letters("zettygjpcoedwyio")
-    assert  NiceNaughty.has_consecutive_letters("xobfthcuuzhvhzpn")
-    assert !NiceNaughty.has_consecutive_letters("dhfespzrhecigzqb")
-    assert !NiceNaughty.has_consecutive_letters("arsvmfznblsqngvb")
-    assert !NiceNaughty.has_consecutive_letters("iyphmphgntinfezg")
-    assert !NiceNaughty.has_consecutive_letters("cgpaqjvzhbumckwo")
+    assert  NiceNaughty.has_consecutive_letters?("eytquncjituzzhsx")
+    assert  NiceNaughty.has_consecutive_letters?("dtfkgggvqadhqbwb")
+    assert  NiceNaughty.has_consecutive_letters?("zettygjpcoedwyio")
+    assert  NiceNaughty.has_consecutive_letters?("xobfthcuuzhvhzpn")
+    assert !NiceNaughty.has_consecutive_letters?("dhfespzrhecigzqb")
+    assert !NiceNaughty.has_consecutive_letters?("arsvmfznblsqngvb")
+    assert !NiceNaughty.has_consecutive_letters?("iyphmphgntinfezg")
+    assert !NiceNaughty.has_consecutive_letters?("cgpaqjvzhbumckwo")
   end
 
   test "doesn't contain ab|cd|pq|xy" do
-    assert !NiceNaughty.has_some_strings("vxrkhvglynljgqrg")
-    assert !NiceNaughty.has_some_strings("bugepxgpgahtsttl")
-    assert NiceNaughty.has_some_strings("odclumkenabcsfzr")
-    assert NiceNaughty.has_some_strings("anoqkkbcdropskhj")
-    assert NiceNaughty.has_some_strings("xycjvvsuaxsbrqal")
-    assert NiceNaughty.has_some_strings("zufmkmuujavcskpq")
+    assert !NiceNaughty.has_some_strings?("vxrkhvglynljgqrg")
+    assert !NiceNaughty.has_some_strings?("bugepxgpgahtsttl")
+    assert NiceNaughty.has_some_strings?("odclumkenabcsfzr")
+    assert NiceNaughty.has_some_strings?("anoqkkbcdropskhj")
+    assert NiceNaughty.has_some_strings?("xycjvvsuaxsbrqal")
+    assert NiceNaughty.has_some_strings?("zufmkmuujavcskpq")
   end
 
   test "string is nice" do
-    func = NiceNaughty.is_nice
+    func = NiceNaughty.is_nice?(:part1)
     assert  func.("aaa")
     assert  func.("ugknbfddgicrmopn")
   end
 
-  test "string is naughty" do
-    func = NiceNaughty.is_nice
+  test "string is nice part1" do
+    func = NiceNaughty.is_nice?(:part1)
     assert !func.("haegwjzuvuyypxyu")
     assert !func.("jchzalrnumimnmhp")
     assert !func.("dvszwmarrgswjxmb")
   end
+
+  test "has pairs that are not overlapped" do
+     NiceNaughty.has_not_overlapped_pairs?("xyxy")
+     NiceNaughty.has_not_overlapped_pairs?("aabcdefgaa")
+     !NiceNaughty.has_not_overlapped_pairs?("aaa")
+  end
+
+  test "has repeated letter with letter in between" do
+    NiceNaughty.has_repeated_letter_with_middleman?("xyx")
+    NiceNaughty.has_repeated_letter_with_middleman?("abcdefeghi")
+    NiceNaughty.has_repeated_letter_with_middleman?("aaa")
+  end
+
+  test "string is nice part 2" do
+    func = NiceNaughty.is_nice?(:part2)
+    assert  func.("qjhvhtzxzqqjkmpb")
+    assert  func.("xxyxx")
+    assert !func.("uurcxstgmygtbstg")
+    assert !func.("ieodomkazucvgmuy")
   end
 end
